de424ee8935d4544eec3cd74140282ae134d8f5d
r.baeriswyl <r.baeriswyl@a0e5147c-9c31-11dd-87e9-a30ea5446288>
2008-10-27 17:52:56 +0000

    - Added missing Files

---
+++ trunk/Waddu/Forms/UnknownAddonsForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Waddu.Forms
+{
+    partial class UnknownAddonsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCopyToClipboard = new System.Windows.Forms.Button();
+            this.txtUnknownAddons = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(237, 328);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnCopyToClipboard
+            // 
+            this.btnCopyToClipboard.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopyToClipboard.Location = new System.Drawing.Point(318, 328);
+            this.btnCopyToClipboard.Name = "btnCopyToClipboard";
+            this.btnCopyToClipboard.Size = new System.Drawing.Size(102, 23);
+            this.btnCopyToClipboard.TabIndex = 1;
+            this.btnCopyToClipboard.Text = "Copy to Clipboard";
+            this.btnCopyToClipboard.UseVisualStyleBackColor = true;
+            this.btnCopyToClipboard.Click += new System.EventHandler(this.btnCopyToClipboard_Click);
+            // 
+            // txtUnknownAddons
+            // 
+            this.txtUnknownAddons.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUnknownAddons.Location = new System.Drawing.Point(12, 12);
+            this.txtUnknownAddons.Multiline = true;
+            this.txtUnknownAddons.Name = "txtUnknownAddons";
+            this.txtUnknownAddons.Size = new System.Drawing.Size(408, 310);
+            this.txtUnknownAddons.TabIndex = 2;
+            // 
+            // UnknownAddonsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(432, 363);
+            this.Controls.Add(this.txtUnknownAddons);
+            this.Controls.Add(this.btnCopyToClipboard);
+            this.Controls.Add(this.btnClose);
+            this.Name = "UnknownAddonsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Unknown Addons";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnCopyToClipboard;
+        private System.Windows.Forms.TextBox txtUnknownAddons;
+    }
+}
