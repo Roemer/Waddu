fe7e5066e3ab6ef4502fc4dfd8d6c018e752ca94
r.baeriswyl <r.baeriswyl@a0e5147c-9c31-11dd-87e9-a30ea5446288>
2008-10-27 17:52:56 +0000

    - Added missing Files

---
+++ Waddu/Forms/UnknownAddonsForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Waddu.Forms
+{
+    public partial class UnknownAddonsForm : Form
+    {
+        public string UnknownAddonsText
+        {
+            get { return txtUnknownAddons.Text; }
+            set { txtUnknownAddons.Text = value; }
+        }
+
+        public UnknownAddonsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopyToClipboard_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtUnknownAddons.Text);
+        }
+    }
+}
